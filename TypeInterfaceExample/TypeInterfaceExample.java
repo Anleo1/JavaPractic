ba9c617f636239027430719e2b36ee2ec9099d76
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-12 22:37:49 +0300

    Create TypeInterfaceExample.java

---
+++ TypeInterfaceExample/TypeInterfaceExample.java
@@ -0,0 +1,6 @@
+public class TypeInferenceExample {
+    public static void main(String[] args) {
+        var name = "Анастасия";
+        System.out.println(name + " тип переменной name: " + name.getClass().getName());
+    }
+}
