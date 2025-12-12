950ae0be9b969f09b78a37d85eea01553446535a
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-12 17:54:47 +0300

    Create main.java

---
+++ StringConcatenation/main.java
@@ -0,0 +1,16 @@
+class StringConcatenation {
+    public static void main(String[] args) {
+        String name = "Максим";
+        int age = 30;
+        double height = 1.80;
+        boolean married = false;
+        char bloodType = '1';
+
+        System.out.println(name + ", " + age + " лет, рост " + height + " см" + "\nБрак: " + married + ", Группа крови: " + bloodType + "\n");
+
+        Integer objI = Integer.valueOf(age);
+        Double objD = Double.valueOf(height);
+        String result = "Объекты: " + objI + ", " + objD;
+        System.out.println(result);
+    }
+}
