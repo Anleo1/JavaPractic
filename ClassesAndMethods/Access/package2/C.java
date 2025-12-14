1539d7cbb40048558c23ecedaf6652aebffad408
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-15 02:46:50 +0300

    Update C.java

--- ClassesAndMethods/Access/package2/C.java
+++ ClassesAndMethods/Access/package2/C.java
@@ -4,7 +4,7 @@ import package1.A;
 public class C {
     public void testC() {
         A obj = new A();
-        System.out.println("OtherClass видит: " + obj.publicVar);
+        System.out.println("C видит: " + obj.publicVar);
         // protectedVar, defaultVar, privateVar - не видно
     }
 }
