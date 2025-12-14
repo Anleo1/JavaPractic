b21bae8cabbc1c66d33646d20a677db8d8b23a5e
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-15 02:44:45 +0300

    Update TestAccess.java

--- ClassesAndMethods/Access/TestAccess.java
+++ ClassesAndMethods/Access/TestAccess.java
@@ -4,13 +4,11 @@ import package2.C;
 public class TestAccess {
     public static void main(String[] args) {
         A obj1 = new A();
-        System.out.println("main видит public: " + obj1.publicVar);
-        obj1.showAll();
         B obj2 = new B();
         C obj3 = new C();
+        System.out.println("main видит public: " + obj1.publicVar);
+        obj1.showAll();
         obj2.testB();
         obj3.testC();
-
-
     }
 }
