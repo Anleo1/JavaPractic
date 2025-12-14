abf10cd6709f081ea4714031328ea7f7c0965f45
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-15 02:53:34 +0300

    Update TestAccess.java

--- ClassesAndMethods/Access/TestAccess.java
+++ ClassesAndMethods/Access/TestAccess.java
@@ -6,7 +6,7 @@ public class TestAccess {
         A obj1 = new A();
         B obj2 = new B();
         C obj3 = new C();
-        System.out.println("main видит public: " + obj1.publicVar);
+        System.out.println("main видит public: " + obj1.publicVar); //остальное - ошибка
         obj1.showAll();
         obj2.testB();
         obj3.testC();
