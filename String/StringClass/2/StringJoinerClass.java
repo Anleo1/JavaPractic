005e5db211272e327ac935843e30430815026d9d
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-17 02:24:01 +0300

    Create StringJoinerClass.java

---
+++ String/StringClass/2/StringJoinerClass.java
@@ -0,0 +1,25 @@
+import java.util.StringJoiner;
+
+public class StringJoinerClass {
+    public static void main(String[] args) {
+        // Пустое значение
+        StringJoiner testSJ = new StringJoiner("-");
+        testSJ.setEmptyValue("Empty");
+        System.out.println(testSJ); // Empty
+        // объединение
+        testSJ.add("Один");
+        System.out.println(testSJ); // Один
+        testSJ.add("Два");
+        System.out.println(testSJ);//Один-Два
+
+        StringJoiner contSJ = new StringJoiner(", ", "[", "]");
+        contSJ.add("Считалочка");
+        contSJ.add("Значение");
+        contSJ.add("Числа");
+        System.out.println(contSJ);//[Считалочка, Значение, Числа]
+
+        // объединение двух StringJoiner
+        testSJ.merge(contSJ);
+        System.out.println(testSJ);//Один-Два-Считалочка, Значение, Числа
+    }
+}
