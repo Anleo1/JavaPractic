eb0b548ada00f8cf225331cc68403260004f1fbe
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-17 03:37:06 +0300

    Create Formatting.java

---
+++ String/FormattingData/1/Formatting.java
@@ -0,0 +1,27 @@
+public class Formatting {
+    public static void main(String[] args) {
+        // Пример 1: %s - строковое представление
+        String name = "Anastasia";
+        int course = 3;
+        boolean isStudent = true;
+        double num = 5.3723498;
+        char symbol = 'R';
+
+
+        // %s — строка
+        System.out.printf("Name: %s%n", name);
+
+        // %d — десятичное целое число
+        System.out.printf("Курс: %d%n", course);
+
+        // %b — логическое значение
+        System.out.printf("Student: %b%n", isStudent);
+
+        // %c — символ
+        System.out.printf("Символ: %c%n", symbol);
+
+        // %f — число с плавающей точкой
+        System.out.printf("Число с плавающей точкой: %.4f%n", num);
+
+    }
+}
