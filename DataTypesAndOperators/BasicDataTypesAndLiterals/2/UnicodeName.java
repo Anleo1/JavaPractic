41bbd95214f3a46533bbf54099578314ca9c4533
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-17 15:48:19 +0300

    Update and rename DataTypesAndOperators/BasicDataTypesAndLiterals/2/UnicodeName/main.java to DataTypesAndOperators/BasicDataTypesAndLiterals/2/UnicodeName.java

--- DataTypesAndOperators/BasicDataTypesAndLiterals/2/UnicodeName.java
+++ DataTypesAndOperators/BasicDataTypesAndLiterals/2/UnicodeName.java
@@ -1,4 +1,4 @@
-class Name {
+public class UnicodeName {
     public static void main(String[] args) {
         System.out.println("Моё имя в Unicode:");
         System.out.println("\u0410\u043D\u0430\u0441\u0442\u0430\u0441\u0438\u044F");
